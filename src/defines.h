2fd469c5f6f1a50c78deca4e7af787e9dcbd1e1f
Stefan <Matthias.Stefan@loewen.de>
2025-05-08 11:04:46 +0200

    add HUB75 pinout struct, enums and factory for Adafruit Bonnet

---
+++ src/defines.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <stdbool.h>
+
+// unsigned int types.
+
+/** @brief unsigned 8-bit integer */
+typedef unsigned char u8;
+
+/** @brief unsigned 16-bit integer */
+typedef unsigned short u16;
+
+/** @brief unsigned 32-bit integer */
+typedef unsigned int u32;
+
+/** @brief unsigned 64-bit integer */
+typedef unsigned long long u64;
+
+// signed int types.
+
+/** @brief signed 8-bit integer */
+typedef signed char s8;
+
+/** @brief signed 16-bit integer */
+typedef signed short s16;
+
+/** @brief signed 32-bit integer */
+typedef signed int s32;
+
+/** @brief signed 64-bit integer */
+typedef signed long long s64;
+
+// floating point types
+
+/** @brief 32-bit floating point number */
+typedef float f32;
+
+/** @brief 64-bit floating point number */
+typedef double f64;
+
+// boolean types
+
+/** @brief 32-bit boolean type, used for APIs which require it */
+typedef int b32;
+
+/** @brief 8-bit boolean type */
+typedef bool b8;
---
+++ src/panel_config.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "defines.h"
+
+/** @brief */
+typedef u8 gpio_pin;
+
+/** @brief */
+typedef enum _rgb_index_e
+{
+    R1 = 0,
+    G1,
+    B1,
+    R2,
+    G2,
+    B2,
+
+    RGB_INDEX_MAX
+} rgb_index_e;
+
+/** @brief */
+typedef enum _addr_index_e {
+    A = 0,
+    B,
+    C,
+    D,
+    E,
+
+    ADDR_INDEX_MAX
+} addr_index_e;
+
+/** @brief */
+typedef struct _hub75_adafruit_bonnet_pinout_s 
+{
+    gpio_pin pin_rgb[RGB_INDEX_MAX];
+    gpio_pin pin_addr[ADDR_INDEX_MAX];  
+
+    gpio_pin pin_oe;
+    gpio_pin pin_clk;
+    gpio_pin pin_lat;
+
+    u32 clk_bit;
+    u32 lat_bit;
+    u32 oe_bit;
+
+    u32 oe_active;
+    u32 oe_inactive;
+
+    u32 post_oe_delay;
+    u32 post_latch_delay;
+    u32 post_addr_delay;
+} hub75_adafruit_bonnet_pinout_s;
+
+/** @brief */
+hub75_adafruit_bonnet_pinout_s create_adafruit_bonnet_pinout();
